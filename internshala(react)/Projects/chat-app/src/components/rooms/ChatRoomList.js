2dd894c9d0a535d55b2a3fdb8f1d973a833a5f06
kunalsjaswal <kunal23singh0@gmail.com>
2022-07-12 20:31:53 +0530

    link highlighted

--- internshala(react)/Projects/chat-app/src/components/rooms/ChatRoomList.js
+++ internshala(react)/Projects/chat-app/src/components/rooms/ChatRoomList.js
@@ -1,24 +1,42 @@
 import React from "react";
-import { Nav } from "rsuite";
-import RoomItem from './RoomItem'
+import { Loader, Nav } from "rsuite";
+import RoomItem from "./RoomItem";
 import { ChatRoomStyle } from "../../styled/MainStyle";
-const ChatRoomList = ({aboveElHeight}) => {
+import { useRooms } from "../../context/rooms.context";
+import { Link, useLocation } from "react-router-dom";
+const ChatRoomList = ({ aboveElHeight }) => {
+  const rooms = useRooms();
+  const location = useLocation();
+
   return (
     <ChatRoomStyle>
-    <Nav
-      appearance="subtle"
-      vertical
-      reversed
-      className="overflow-y-scroll custom-scroll"
-      style={{
-        overflowY:"scroll",
-        height:`calc(100% - ${aboveElHeight}px)`,
-    }}
-    >
-      <Nav.Item>
-        <RoomItem/>
-      </Nav.Item>
-    </Nav>
+      <Nav
+        appearance="subtle"
+        vertical
+        reversed
+        className="overflow-y-scroll custom-scroll"
+        style={{
+          overflowY: "scroll",
+          height: `calc(100% - ${aboveElHeight}px)`,
+        }}
+        activeKey={location.pathname} //this is for active link highlighter
+      >
+        {!rooms && (
+          <Loader center vertical content="Loading" speed="slow" size="md" />
+        )}
+        {rooms &&
+          rooms.length > 0 &&
+          rooms.map((room) => (
+            <Nav.Item
+              componentClass={Link}
+              to={`/chat/${room.id}`}
+              key={room.id}
+              eventKey={`/chat/${room.id}`} //this is for active link highlighter
+            >
+              <RoomItem room={room} />
+            </Nav.Item>
+          ))}
+      </Nav>
     </ChatRoomStyle>
   );
 };
--- internshala(react)/Projects/chat-app/src/components/rooms/RoomItem.js
+++ internshala(react)/Projects/chat-app/src/components/rooms/RoomItem.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import TimeAgo from 'timeago-react'
-const RoomItem = () => {
+const RoomItem = ({room}) => {
 
-
+const {createdAt,name}=room;
   
   return (
     <div>
 
       <div className='d-flex justify-content-between align-items-center'>
-        <h3 className='text-disappear'>Room name</h3>
-        <TimeAgo dateTime={new Date()} className="font-normal text-black-4z"/>
+        <h3 className='text-disappear'>{name}</h3>
+        <TimeAgo datetime={new Date(createdAt)} className="font-normal text-black-4z"/>
       </div>
 
       <div className="d-flex align-items-center text-black-70">
