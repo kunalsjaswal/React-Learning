4c3979a3600a8ba240ba0c153bca9097cd0a8b48
kunalsjaswal <kunal23singh0@gmail.com>
2022-07-13 21:12:54 +0530

    last messages

--- internshala(react)/Projects/chat-app/src/components/rooms/RoomItem.js
+++ internshala(react)/Projects/chat-app/src/components/rooms/RoomItem.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import TimeAgo from 'timeago-react'
-const RoomItem = ({room}) => {
-
-const {createdAt,name}=room;
-  
+import React from "react";
+import TimeAgo from "timeago-react";
+import ProfileAvatar from "../profileAvatar";
+const RoomItem = ({ room }) => {
+  const { createdAt, name, lastMessage } = room;
   return (
     <div>
-
-      <div className='d-flex justify-content-between align-items-center'>
-        <h3 className='text-disappear'>{name}</h3>
-        <TimeAgo datetime={new Date(createdAt)} className="font-normal text-black-4z"/>
+      <div className="d-flex justify-content-between align-items-center">
+        <h3 className="text-disappear">{name}</h3>
+        <TimeAgo
+          datetime={
+            lastMessage ? new Date(lastMessage.createdAt) : new Date(createdAt)
+          }
+          className="font-normal text-black-45"
+        />
       </div>
 
       <div className="d-flex align-items-center text-black-70">
-        <span>No messages yet...</span>
+        {lastMessage ? (
+          <>
+            <div className="d-flex align-items-center">
+              <ProfileAvatar
+                src={lastMessage.author.avatar}
+                name={lastMessage.author.name}
+                size="sm"
+              />
+            </div>
+
+            <div className="text-disappear ml-2">
+              <div className="italic">{lastMessage.author.name}</div>
+              <span>{lastMessage.text}</span>
+            </div>
+          </>
+        ) : (
+          <span>No messages yet...</span>
+        )}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default RoomItem
+export default RoomItem;
