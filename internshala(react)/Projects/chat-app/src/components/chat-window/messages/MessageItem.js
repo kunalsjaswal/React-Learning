f923b37fe812a4fc705c2b0536c10486bab13db7
kunalsjaswal <kunal23singh0@gmail.com>
2022-07-16 12:46:31 +0530

    on chat hover

--- internshala(react)/Projects/chat-app/src/components/chat-window/messages/MessageItem.js
+++ internshala(react)/Projects/chat-app/src/components/chat-window/messages/MessageItem.js
@@ -6,10 +6,13 @@ import ProfileAvatar from "../../profileAvatar";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
 import {useCurrentRoom} from '../../../context/current-room-context'
 import { auth } from "../../../misc/firebase";
+import { useHover } from "../../../misc/customHooks";
 
 const MessageItem = ({message,handleAdmin}) => {
   
     const {author,createdAt,text}=message;
+
+    const [selfRef,isHover]=useHover();
 
     const isAdmin=useCurrentRoom(v=>v.isAdmin);
     const admins=useCurrentRoom(v=>v.admins);
@@ -21,7 +24,7 @@ const MessageItem = ({message,handleAdmin}) => {
 
     
   return (
-   <li className="padded mb-1">
+   <li className={`padded mb-1 cursor-pointer ${isHover?'bg-black-02':''}`} ref={selfRef}>
 
     <div className="d-flex align-items-center font-bolder mb-1">
 
--- internshala(react)/Projects/chat-app/src/misc/customHooks.js
+++ internshala(react)/Projects/chat-app/src/misc/customHooks.js
@@ -1,51 +1,75 @@
-import { useEffect,useState,useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import { database } from "./firebase";
 
+export function useModalState(defaultVal = false) {
+  const [isOpen, setIsOpen] = useState(defaultVal);
+  const open = useCallback(() => setIsOpen(true));
+  const close = useCallback(() => setIsOpen(false));
 
-export function useModalState(defaultVal=false){
-
-    const [isOpen, setIsOpen] = useState(defaultVal)
-    const open=useCallback(()=> setIsOpen(true));
-    const close=useCallback(()=> setIsOpen(false));
-
-    return {isOpen,open,close}
+  return { isOpen, open, close };
 }
 
-export const useMediaQuery = query => {
-    const [matches, setMatches] = useState(
-      () => window.matchMedia(query).matches
-    );
-  
-    useEffect(() => {
-      const queryList = window.matchMedia(query);
-      setMatches(queryList.matches);
-  
-      const listener = evt => setMatches(evt.matches);
-  
-      queryList.addListener(listener);
-      return () => queryList.removeListener(listener);
-    }, [query]);
-  
-    return matches;
-  };
+export const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
 
-  export function usePresence(uid){
-    const [presence, setPresence] = useState(null)
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+    setMatches(queryList.matches);
 
-    useEffect(()=>{
-      const userStatusRef=database.ref(`/status/${uid}`);
-      userStatusRef.on('value',(snap)=>{
-        if(snap.exists()){
-          const data=snap.val();
-          
-          setPresence(data)
-        }
-      })
+    const listener = (evt) => setMatches(evt.matches);
 
-      return ()=>{
-        userStatusRef.off()
+    queryList.addListener(listener);
+    return () => queryList.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
+
+export function usePresence(uid) {
+  const [presence, setPresence] = useState(null);
+
+  useEffect(() => {
+    const userStatusRef = database.ref(`/status/${uid}`);
+    userStatusRef.on("value", (snap) => {
+      if (snap.exists()) {
+        const data = snap.val();
+
+        setPresence(data);
       }
-    },[uid])
+    });
 
-    return presence;
-  }
+    return () => {
+      userStatusRef.off();
+    };
+  }, [uid]);
+
+  return presence;
+}
+
+export function useHover() {
+  const [value, setValue] = useState(false);
+
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  useEffect(() => {
+    const node = ref.current;
+
+    if (node) {
+      node.addEventListener("mouseover", handleMouseOver);
+      node.addEventListener("mouseout", handleMouseOut);
+    }
+
+    return () => {
+      node.removeEventListener("mouseover", handleMouseOver);
+      node.removeEventListener("mouseout", handleMouseOut);
+    };
+
+  },[ref.current]);
+
+  return [ref,value]
+}
