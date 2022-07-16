0878bd247c0cdc1334eeef30ec9767e3d0d6c850
kunalsjaswal <kunal23singh0@gmail.com>
2022-07-16 19:11:35 +0530

    mic option

---
+++ internshala(react)/Projects/chat-app/src/components/chat-window/bottom/AudioMsgBtn.js
@@ -0,0 +1,55 @@
+import React, { useCallback, useState } from "react";
+import { InputGroup, Icon,Alert } from "rsuite";
+import { ReactMic } from "react-mic";
+import { storage } from "../../../misc/firebase";
+import { useParams } from "react-router";
+
+const AudioMsgBtn = ({ afterUpload }) => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [isUploading, setIsUploading] = useState(false)
+
+  const {chatId}=useParams();
+  const onUpload = useCallback(async (data) => {
+    setIsUploading(true);
+    try {
+      const snap = await storage
+        .ref(`/chat/${chatId}`)
+        .child(`audio_${Date.now()}.mp3`)
+        .put(data.blob, { 
+            cacheControl: `public,max-age=${3600 * 24 * 3}` 
+        });
+
+        const file={
+            contentType: snap.metadata.contentType,
+            name: snap.metadata.name,
+            url: await snap.ref.getDownloadURL()
+        };
+
+        setIsUploading(false)
+        afterUpload([file])
+            
+    } catch (error) {
+        setIsUploading(false)
+        Alert.error(error.message,4000)
+    }
+  }, [afterUpload,chatId]);
+
+  const onClick = useCallback(() => {
+    setIsRecording((p) => !p);
+  }, []);
+  return (
+    <>
+      <InputGroup.Button onClick={onClick} disabled={isUploading} className={isRecording ? 'animate-blink':''}>
+        <Icon icon="microphone" />
+        <ReactMic
+          record={isRecording}
+          className="d-none"
+          onStop={onUpload}
+          mimeType="audio/mp3 "
+        />
+      </InputGroup.Button>
+    </>
+  );
+};
+
+export default AudioMsgBtn;
